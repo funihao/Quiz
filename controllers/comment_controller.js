19bb25d4495676a200c3ae3c199f0c4850e29545
jjpalacios <funihao@gmail.com>
2015-07-26 01:06:21 +0200

    Moderación de comentarios

--- controllers/comment_controller.js
+++ controllers/comment_controller.js
@@ -1,5 +1,18 @@
 var models = require('../models/models');
 
+// Autoload - :id comentarios
+exports.load = function (req, res, next, commentId) {
+  models.Comment.find({
+    where: {id: Number(commentId)}})
+    .then(function (comment) {
+      if (comment) {
+        req.comment = comment;
+        next();
+      } else {next(new Error('No existe commentId=' + commentId))}
+    })
+    .catch(function (error) {next(error)});
+};
+
 //GET /quizes/:quizId/comments/new
 exports.new = function (req, res) {
   res.render('comments/new', {quizId: req.params.quizId, errors: []});
@@ -24,5 +37,14 @@ exports.create = function (req, res) {
           .then(function () {res.redirect('/quizes/' + req.params.quizId)});
       }
     })
+    .catch(function (error) {next(error)});
+};
+
+// GET /quizes/:quizId/:commentId/publish
+exports.publish = function (req, res) {
+  req.comment.publicado = true;
+
+  req.comment.save({fields: ["publicado"]})
+    .then(function () {res.redirect('/quizes/' + req.params.quizId);})
     .catch(function (error) {next(error)});
 };
--- routes/index.js
+++ routes/index.js
@@ -12,6 +12,7 @@ router.get('/', function(req, res) {
 
 // Autoload de comandos con :quizId
 router.param('quizId', quizController.load);
+router.param('commentId', commentController.load);
 
 // Definición de rutas de sesión
 router.get('/login', sessionController.new);
@@ -31,6 +32,8 @@ router.delete('/quizes/:quizId(\\d+)', sessionController.loginRequired, quizCont
 // Definición de rutas para /comments
 router.get('/quizes/:quizId(\\d+)/comments/new', commentController.new);
 router.post('/quizes/:quizId(\\d+)/comments', commentController.create);
+router.put('/quizes/:quizId(\\d+)/comments/:commentId(\\d+)/publish',
+            sessionController.loginRequired, commentController.publish);
 
 // Ruta créditos
 router.get('/author', quizController.author);
